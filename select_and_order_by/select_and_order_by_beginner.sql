5038da46a5c0990daa2c5aa025419c6e20d39b1f
Francisco Gomes <fgsj.dev@outlook.com>
2025-09-10 16:27:27 -0300

    Adjustment space blank in head description problem

--- select_and_order_by/select_and_order_by_beginner.sql
+++ select_and_order_by/select_and_order_by_beginner.sql
@@ -103,7 +103,7 @@ SELECT TOP 10 *
 FROM Sales.SalesOrderHeader as ssoh 
 ORDER BY ssoh.TotalDue DESC
 
--- exercise 22: return all coins (Name, CurrencyCode)in table "Sales.Currency"
+-- exercise 22: return all coins (Name, CurrencyCode) in table "Sales.Currency"
 SELECT sc.[Name] as 'Nome moeda', sc.CurrencyCode as 'Codigo moeda'
 FROM Sales.Currency as sc
 ORDER BY sc.[Name] ASC -- Ordering name based in name coin 
