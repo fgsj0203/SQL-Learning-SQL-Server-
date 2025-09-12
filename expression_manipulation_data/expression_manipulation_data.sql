24e5351b3a8e6ecf78e704457ee7f2b779b9f426
Francisco Gomes <fgsj.dev@outlook.com>
2025-09-12 20:15:11 -0300

    update file of type text with question and first query exercise

--- expression_manipulation_data/expression_manipulation_data.sql
+++ expression_manipulation_data/expression_manipulation_data.sql
@@ -8,3 +8,6 @@ Description: file with exercises beginner a use expression for manipulation data
 Database: AdventureWorks2022
 */
 
+-- exercise 01: return names and price list round for number integer
+SELECT ppro.Name as 'Nome do produto', ROUND (ppro.ListPrice, 0) as 'Valor arredondado'
+FROM Production.Product as ppro
