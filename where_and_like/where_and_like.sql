5b7b2873cc15bff399444207c4eefdfecb51f9b8
Francisco Gomes <fgsj.dev@outlook.com>
2025-09-12 17:22:38 -0300

    creating head data of file

---
+++ where_and_like/where_and_like.sql
@@ -0,0 +1,10 @@
+/*
+Author: Francisco Gomes da Silva Junior
+College: Estacio de Sa
+Course: Computer Science
+Period: 1
+Date: 12/09/2025 - in format Brazil
+Description: file with exercises beginner with clausules "WHERE and LIKE"
+Database: AdventureWorks2022
+*/
+
