625eb4adc7e542778e00a11cd7cb854cec47cfc8
Francisco Gomes <fgsj.dev@outlook.com>
2025-09-16 10:44:21 -0300

    Head data personal project and script for search directly column

---
+++ AdventureWorksLT2022/select_and_where_beginner.sql
@@ -0,0 +1,25 @@
+/*
+Author: Francisco Gomes da Silva Junior
+College: Estacio de Sa
+Course: Computer Science
+Period: 1
+Date: 15/09/2025 - in format Brazil
+Description: file with exercises beginner of clausule "SELECT and WHERE"
+Database: AdventureWorksLT2022
+*/
+
+
+
+
+-- Script for search column directly in tables
+SELECT
+    TABLE_NAME,
+    COLUMN_NAME,
+    DATA_TYPE,
+    CHARACTER_MAXIMUM_LENGTH
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    COLUMN_NAME = 'Amount'
+-------------------------------------------------
+
